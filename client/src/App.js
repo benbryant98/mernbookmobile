26f2045a970a522905f1d78cdf07cd34863625e2
Benjamin Bryant <bryantbc98@gmail.com>
2023-07-23 22:00:59 -0400

    updated server.js to start apollo sever

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemmoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const client = new ApolloClient({
   uri: "/graphql",
-  cache: new InMemmoryCache(),
+  cache: new InMemoryCache(),
 });
 
 function App() {
